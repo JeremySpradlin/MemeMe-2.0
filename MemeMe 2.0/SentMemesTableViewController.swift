dfb39789c3baaa8cf8f163bf267328e64b98af33
Jeremy Spradlin <spradlinjk@gmail.com>
2017-10-20 19:49:36 -0400

    Can't remember what I added but added stuff

--- MemeMe 2.0/SentMemesTableViewController.swift
+++ MemeMe 2.0/SentMemesTableViewController.swift
@@ -22,20 +22,40 @@ class SentMemesTableViewController: UITableViewController {
         memes = appDelegate.memes
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func viewWillAppear(_ animated: Bool) {
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            print(self.memes.count)
+            return self.memes.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "memeCell")!
+            let meme = self.memes[(indexPath as NSIndexPath).row]
+            
+            cell.textLabel?.text = meme.topTextField
+            cell.imageView?.image = meme.memedImage
+            
+            return cell
+        }
+        print("Inside View Will Appear")
         print(self.memes.count)
-        return self.memes.count
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "memeCell")!
-        let meme = self.memes[(indexPath as NSIndexPath).row]
-        
-        cell.textLabel?.text = meme.topTextField
-        cell.imageView?.image = meme.memedImage
-        
-        return cell
-    }
+    
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        print(self.memes.count)
+//        return self.memes.count
+//    }
+//
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "memeCell")!
+//        let meme = self.memes[(indexPath as NSIndexPath).row]
+//
+//        cell.textLabel?.text = meme.topTextField
+//        cell.imageView?.image = meme.memedImage
+//
+//        return cell
+//    }
     
     @IBAction func addMemeButton(_ sender: Any) {
         let vc = self.storyboard?.instantiateViewController(withIdentifier: "MemeEditorView") as! MemeEditorViewController
