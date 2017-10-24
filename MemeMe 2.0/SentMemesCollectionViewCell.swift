493c2116386d5f024eaa5f0715ec3e0d9262d5e2
Jeremy Spradlin <spradlinjk@gmail.com>
2017-10-24 08:18:14 -0400

    Added image view to collection cell and collection cell sub class

---
+++ MemeMe 2.0/SentMemesCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SentMemesCollectionViewCell.swift
+//  MemeMe 2.0
+//
+//  Created by Jeremy Spradlin on 10/24/17.
+//  Copyright © 2017 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class SentMemesCollectionViewCell: UICollectionViewCell {
+    
+    
+    
+}
--- MemeMe 2.0/SentMemesCollectionViewController.swift
+++ MemeMe 2.0/SentMemesCollectionViewController.swift
@@ -6,20 +6,34 @@
 //  Copyright © 2017 Udacity. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
 class SentMemesCollectionViewController: UICollectionViewController {
     
-    var memes: [Meme]!
-    
+    var memes: [Meme] { return (UIApplication.shared.delegate as! AppDelegate).memes }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        memes = appDelegate.memes
     }
     
     @IBOutlet weak var addMemeButton: UIBarButtonItem!
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return memes.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        //let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "memeCollectionCell")!
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "memeCell", for: indexPath)
+        let meme = self.memes[(indexPath as NSIndexPath).row]
+        
+        
+        
+//        cell.imageView?.image = meme.memedImage
+//        cell.textLabel?.text = meme.topTextField
+        return cell
+
+    }
     
     @IBAction func addMemeButton(_ sender: Any) {
         let vc = self.storyboard?.instantiateViewController(withIdentifier: "MemeEditorView") as! MemeEditorViewController
