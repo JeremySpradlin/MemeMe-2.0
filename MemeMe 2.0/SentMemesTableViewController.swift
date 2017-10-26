911133f90e109638c23129536d6b4d56d07b5551
Jeremy Spradlin <spradlinjk@gmail.com>
2017-10-26 13:17:00 -0400

    Removed edit button from tableview / Code Cleanup and comments

--- MemeMe 2.0/SentMemesTableViewController.swift
+++ MemeMe 2.0/SentMemesTableViewController.swift
@@ -20,11 +20,13 @@ class SentMemesTableViewController: UITableViewController {
     
     //MARK: Override Functions
     override func viewWillAppear(_ animated: Bool) {
+        //Reloads tabledata when view reappears at the top of the stack
         tableView.reloadData()
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.memes.count
     }
+    //Creates a cell for each object in the data and adds it to the tableview
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "memeCell")!
         let meme = self.memes[(indexPath as NSIndexPath).row]
@@ -32,21 +34,17 @@ class SentMemesTableViewController: UITableViewController {
         cell.imageView?.image = meme.memedImage
         return cell
     }
+    //Allows the user to swipe to delete a row from the data view as well as the data source
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             let object = UIApplication.shared.delegate
             let appDelegate = object as! AppDelegate
             appDelegate.memes.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
     }
     
     //MARK: Action Functions
-    @IBAction func editButton(_ sender: Any) {
-
-    }
     @IBAction func addMemeButton(_ sender: Any) {
         let vc = self.storyboard?.instantiateViewController(withIdentifier: "MemeEditorView") as! MemeEditorViewController
         present(vc, animated: true, completion: nil)
